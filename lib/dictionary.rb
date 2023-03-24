bbe6e383c1ef7bdfe821cd7be6417e62ec275d58
Ilya Eletskiy <ilarelet@gmail.com>
2023-03-24 14:12:04 +0300

    random word selection method added

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -2,10 +2,17 @@ class Dictionary
     attr_reader :words, :size
 
     def initialize(dict_file)
-        @words = Hash.new
-        File.open(dict_file, 'r').readlines.each do |word|
-            @words[word] = word.length
+        @words = Array.new
+        File.open(dict_file, 'r') do |file|
+            file.readlines.each do |word|
+                @words.push word 
+            end
         end
-        @size = @words.length
     end
+
+    def random_word
+        #Choose a random word between 5 and 12 characters long
+        @words.select{|word| word.length >= 5 and word.length <= 12}.sample
+    end
+
 end

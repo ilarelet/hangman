b7e8f70d9a91a79cc14000b3bda788ac11bd38a9
Ilya Eletskiy <ilarelet@gmail.com>
2023-03-24 15:05:34 +0300

    Display status added

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,6 @@
 require_relative 'dictionary.rb'
 class Game
-    attr_accessor :used_letters, :secret_word, :guessed, :turn
+    attr_accessor :used_letters, :secret_word, :guessed, :mistakes
 
     def initialize(dictionary)
         #select a random secret word from the doctionary
@@ -12,19 +12,22 @@ class Game
         end
         #the array of used letters will help the players keep track of what they already tried
         @used_letters = []
-        @turn = 1
+        @mistakes = 0
         #display the "new game" message
         self.start_msg
     end
     
     def start_msg
-        puts 'A new game begins!'
-        puts
-        puts 'In this game you need to guess an English word we randomly chose for you.'
-        self.display_guess
+        puts "A new game begins!\n"
+        puts "In this game you need to guess an English word we randomly chose for you. Good luck!\n"
+        self.display_status
+    end
     end
 
-    def display_guess
-        puts @guessed.join(' ')
+    def display_status
+        puts "#{@mistakes} out of 7 mistakes made.\n" unless  @mistakes == 0
+        puts "You've used the following letters: #{@used_letters.join(', ')}\n" unless  @used_letters == []
+        puts "The word: |#{@guessed.join(' ')}|\n"
+        puts 'Guess a letter: '
     end
 end
